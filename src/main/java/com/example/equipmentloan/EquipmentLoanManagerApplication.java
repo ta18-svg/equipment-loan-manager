92a10bd4df7a2192ddc77fd34a934d801cbf4b5d
ta18-svg <t-ibara@ibataku.com>
2026-02-08 09:30:01 +0900

    EquipmentLoanManagerApplication.java作成

---
+++ src/main/java/com/example/equipmentloan/EquipmentLoanManagerApplication.java
@@ -0,0 +1,11 @@
+package com.example.equipmentloan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EquipmentLoanManagerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(EquipmentLoanManagerApplication.class, args);
+    }
+}
